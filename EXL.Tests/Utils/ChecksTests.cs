64cad9e84da4a3f32fb2d3e1fc897e344892161c
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2024-09-18 18:19:12 +0300

    Add conversion validation
    Added a convertion validation method for doubles, to avoid code duplication and shotgun surgery

---
+++ EXL.Tests/Utils/ChecksTests.cs
@@ -0,0 +1,42 @@
+﻿using EXL.Utils;
+
+namespace EXL.Tests.Utils
+{
+    [TestFixture]
+    public class ChecksTests
+    {
+        [Test]
+        public void CanConvertToDouble_WithValidDoubleString_ReturnsTrue()
+        {
+            double result = 0;
+            Assert.DoesNotThrow(() => Checks.CanConvertToDouble("123.45", out result));
+            Assert.That(result, Is.EqualTo(123.45));
+        }
+
+        [Test]
+        public void CanConvertToDouble_WithValidIntString_ReturnsTrue()
+        {
+            double result = 0;
+            Assert.DoesNotThrow(() => Checks.CanConvertToDouble("123", out result));
+            Assert.That(result, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void CanConvertToDouble_WithInvalidString_ThrowsInvalidCastException()
+        {
+            Assert.Throws<InvalidCastException>(() => Checks.CanConvertToDouble("invalid", out double result));
+        }
+
+        [Test]
+        public void CanConvertToDouble_WithNullValue_ThrowsInvalidCastException()
+        {
+            Assert.Throws<InvalidCastException>(() => Checks.CanConvertToDouble(null, out double result));
+        }
+
+        [Test]
+        public void CanConvertToDouble_WithNonNumericObject_ThrowsInvalidCastException()
+        {
+            Assert.Throws<InvalidCastException>(() => Checks.CanConvertToDouble(new object(), out double result));
+        }
+    }
+}
---
+++ EXL/Utils/Checks.cs
@@ -0,0 +1,25 @@
+﻿namespace EXL.Utils
+{
+    /// <summary>
+    /// Provides utility methods for various checks and validations.
+    /// </summary>
+    public static class Checks
+    {
+        /// <summary>
+        /// Attempts to convert the specified value to a double.
+        /// </summary>
+        /// <param name="value">The value to be converted to a double.</param>
+        /// <param name="result">When this method returns, contains the double value equivalent to the value contained in <paramref name="value"/>, if the conversion succeeded, or zero if the conversion failed. This parameter is passed uninitialized.</param>
+        /// <exception cref="InvalidCastException">Thrown when the provided value cannot be converted to a double.</exception>
+        /// <remarks>
+        /// This method uses <see cref="double.TryParse(string, out double)"/> to attempt the conversion. If the conversion fails, an <see cref="InvalidCastException"/> is thrown.
+        /// </remarks>
+        public static void CanConvertToDouble(object value, out double result)
+        {
+            if (!double.TryParse(value.ToString(), out result))
+            {
+                throw new InvalidCastException("The provided value cannot be converted to a double.");
+            }
+        }
+    }
+}
