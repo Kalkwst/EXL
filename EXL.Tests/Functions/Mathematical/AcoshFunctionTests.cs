ad8ca3dc8f85b3f71269d81bcda105573264f120
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2024-09-21 10:39:50 +0300

    :art: Fix naming violation and IDE suggestions for the test

--- EXL.Tests/Functions/Mathematical/AcoshFunctionTests.cs
+++ EXL.Tests/Functions/Mathematical/AcoshFunctionTests.cs
@@ -5,44 +5,44 @@ namespace EXL.Tests.Functions.Mathematical
     [TestFixture]
     public class AcoshFunctionTests
     {
-        private AcoshFunction _acoshFunction;
-        private static readonly object[] args = new object[] { };
+        private AcoshFunction acoshFunction;
+        private static readonly object[] Args = [];
 
         [SetUp]
         public void SetUp()
         {
-            _acoshFunction = new AcoshFunction();
+            acoshFunction = new AcoshFunction();
         }
 
         [Test]
         public void Execute_WithValidNumber_ReturnsAcoshValue()
         {
-            var result = _acoshFunction.Execute(new object[] { 1.5 });
+            var result = acoshFunction.Execute([1.5]);
             Assert.That(result, Is.EqualTo(Math.Acosh(1.5)));
         }
 
         [Test]
         public void Execute_WithNumberLessThanOne_ThrowsInvalidOperationException()
         {
-            Assert.Throws<InvalidOperationException>(() => _acoshFunction.Execute(new object[] { 0.5 }));
+            Assert.Throws<InvalidOperationException>(() => acoshFunction.Execute([0.5]));
         }
 
         [Test]
         public void Execute_WithInvalidType_ThrowsInvalidOperationException()
         {
-            Assert.Throws<InvalidOperationException>(() => _acoshFunction.Execute(new object[] { "invalid" }));
+            Assert.Throws<InvalidOperationException>(() => acoshFunction.Execute(["invalid"]));
         }
 
         [Test]
         public void Execute_WithMultipleArguments_ThrowsInvalidOperationException()
         {
-            Assert.Throws<InvalidOperationException>(() => _acoshFunction.Execute(new object[] { 1.5, 2.0 }));
+            Assert.Throws<InvalidOperationException>(() => acoshFunction.Execute([1.5, 2.0]));
         }
 
         [Test]
         public void Execute_WithEmptyArguments_ThrowsInvalidOperationException()
         {
-            Assert.Throws<InvalidOperationException>(() => _acoshFunction.Execute(args));
+            Assert.Throws<InvalidOperationException>(() => acoshFunction.Execute(Args));
         }
     }
 }
