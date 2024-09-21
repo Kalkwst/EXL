df37706c05c031c5db0399e4dc4a87a99fa46820
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2024-09-21 10:36:37 +0300

    :memo: Update documentation of the function

--- EXL/Functions/Mathematical/AcosFunction.cs
+++ EXL/Functions/Mathematical/AcosFunction.cs
@@ -2,9 +2,21 @@ using EXL.Utils;
 
 namespace EXL.Functions.Mathematical
 {
+    /// <summary>
+    /// Represents a function that calculates the arccosine of a number.
+    /// </summary>
     public class AcosFunction : IFunction
     {
-
+        /// <summary>
+        /// Executes the ACOS function with the specified arguments.
+        /// </summary>
+        /// <param name="args">An array containing a single numeric argument.</param>
+        /// <returns>The arccosine of the specified number.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when the number of arguments is not exactly one,
+        /// or when the argument is not a numeric value,
+        /// or when the numeric value is not between -1 and 1.
+        /// </exception>
         public object Execute(object[] args)
         {
             if (args.Length != 1)
@@ -12,12 +24,12 @@ namespace EXL.Functions.Mathematical
                 throw new InvalidOperationException("ACOS function requires exactly one argument: number.");
             }
 
-            if (!Checks.TryConvertToDouble(args[0], out var number))
+            if (args[0] is not double number && !Checks.TryConvertToDouble(args[0], out number))
             {
                 throw new InvalidOperationException("ACOS function requires a numeric value.");
             }
 
-            if (number < -1.0 || number > 1.0)
+            if (number is < -1.0 or > 1.0)
             {
                 throw new InvalidOperationException("The number argument for ACOS must be between -1 and 1.");
             }
